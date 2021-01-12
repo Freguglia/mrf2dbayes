00be653e7c2cb8252d6622c7719ac12932ce8a23
Freguglia <victorfreguglia@gmail.com>
2021-01-12 15:34:39 -0300

    Fixed broken hmrbayes output data.frame

--- R/hmrfbayes.R
+++ R/hmrfbayes.R
@@ -2,7 +2,7 @@
 #'
 #' @return A `list` with ...
 #' @export
-hmrfbayes <- function(y, llapprox, nsamples = 1000, 
+hmrfbayes <- function(y, llapprox, nsamples = 1000,
                       z0 = NULL, mu0 = NULL, sigma0 = NULL,
                       theta0 = NULL, sdpriortheta = 1,
                       sdkerneltheta = 0.005, sdpriormu = 10,
@@ -15,15 +15,15 @@ hmrfbayes <- function(y, llapprox, nsamples = 1000,
   # Check and set initial field config if not defined
   if(is.null(z0) | is.null(mu0) | is.null(sigma0) | is.null(theta0)){
     if(verbose) cat("Computing initial values via EM algorithm", "\n")
-    EM <- mrf2d::fit_ghm(Y = y, 
+    EM <- mrf2d::fit_ghm(Y = y,
                          mrfi = mrfi(1) - c(1,0),
                          theta = array(0, dim = c(C+1, C+1, 1)),
                          equal_vars = TRUE,
                          verbose = FALSE)
     if(is.null(z0)) z0 <- EM$Z_pred
-    if(is.null(mu0)) mu0 <- EM$par$mu 
+    if(is.null(mu0)) mu0 <- EM$par$mu
     if(is.null(sigma0)) sigma0 <- EM$par$sigma^2
-    if(is.null(theta0)) 
+    if(is.null(theta0))
       theta0 <- mrf2d::smr_array(mrf2d::fit_pl(EM$Z_pred, mrfi, family)$theta,
                                  family)
     if(verbose) cat("Done!", "\n")
@@ -31,7 +31,7 @@ hmrfbayes <- function(y, llapprox, nsamples = 1000,
 
   if(is.array(theta0)) theta0 <- mrf2d::smr_array(theta0, family)
   dimtheta <- length(theta0)
-  
+
   # Initialize structures to store the results
   # Testing with a fixed theta first, this will break with
   # interactions other than nearest-neighbor.
@@ -49,8 +49,8 @@ hmrfbayes <- function(y, llapprox, nsamples = 1000,
   if(verbose) cat("Running Markov Chain iterations...", "\n")
   for(t in seq_len(nsamples)){
       # Update mus
-      stts <- tapply(as.vector(y), INDEX = as.vector(z_current), 
-          FUN = function(x){c(sum(x), length(x), sum(x^2))}) 
+      stts <- tapply(as.vector(y), INDEX = as.vector(z_current),
+          FUN = function(x){c(sum(x), length(x), sum(x^2))})
       sums <- sapply(stts, "[", 1)
       ns <- sapply(stts, "[", 2)
       mu_current <- rnorm(C+1,
@@ -69,7 +69,7 @@ hmrfbayes <- function(y, llapprox, nsamples = 1000,
 
       # Update latent field
       w2 <- lapply(seq_len(C+1), FUN = function(x){
-          dnorm(y, 
+          dnorm(y,
                 mean = mu_current[x],
                 sd = sqrt(sigma2_current[x]),
                 log = TRUE)
@@ -88,7 +88,7 @@ hmrfbayes <- function(y, llapprox, nsamples = 1000,
       }
       logratio <- llapprox@lafn(zpass, theta_prop) -
         llapprox@lafn(zpass, theta_current) +
-        sum(dnorm(theta_prop, sd = sdpriortheta, log = TRUE)) - 
+        sum(dnorm(theta_prop, sd = sdpriortheta, log = TRUE)) -
         sum(dnorm(theta_current, sd = sdpriortheta, log = TRUE))
       if(log(runif(1)) < logratio) theta_current <- theta_prop
       theta_chain[t,] <- theta_current
@@ -101,21 +101,18 @@ hmrfbayes <- function(y, llapprox, nsamples = 1000,
   dfmus <- as.data.frame(mu_chain)
   colnames(dfmus) <- 0:C
   dfmus <- cbind(t = 1:nsamples, dfmus, par = "mu")
-  
+
   dfsigma <- as.data.frame(sigma2_chain)
   colnames(dfsigma) <- 0:C
   dfsigma <- cbind(t = 1:nsamples, dfsigma, par = "sigma2")
 
   dfpars <- rbind(dfmus, dfsigma)
-  
+
   # Format data.frame with theta
-  dftheta <- cbind(t = 1:nsamples, as.data.frame(theta_chain), 
-                   mrf2d::vec_description(mrfi, family, C))
-  if(family != "onepar"){
-    dftheta <- tidyr::pivot_longer(dftheta, cols = -"t")
-  } else {
-    colnames(dftheta)[2] <- "value"
-  }
+  dftheta <- cbind(t = 1:nsamples, as.data.frame(theta_chain))
+  dftheta <- tidyr::pivot_longer(dftheta, cols = -"t")
+  dftheta <- cbind(dftheta, mrf2d::vec_description(mrfi, family, C))
+  dftheta <- dftheta[,c("t", "position", "interaction", "value")]
 
   out <- list(dfpars = tibble::as_tibble(dfpars),
               dftheta = tibble::as_tibble(dftheta),
