75f34a2d99f2918fc710e3efd7b75c2a91dbe1bb
Freguglia <victorfreguglia@gmail.com>
2022-12-03 16:25:10 -0300

    mrfi encoder and decoders are not exported

--- R/mrfi_coding.R
+++ R/mrfi_coding.R
@@ -1,9 +1,7 @@
-#' @export
 encode_mrfi <- function(vec_included){
   paste0(as.numeric(vec_included), collapse = "")
 }
 
-#' @export
 decode_mrfi <- function(coded_char){
   as.logical(as.integer(strsplit(coded_char, split = "")[[1]]))
 }
